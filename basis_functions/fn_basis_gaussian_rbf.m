eafa4fd5e659df2ff5375166c2c6ae9180ff8a18
hsiuchin-lin <hsiuchin.lin@gmail.com>
2015-07-06 02:45:33 +0800

    Update fn_basis_gaussian_rbf.m

--- basis_functions/fn_basis_gaussian_rbf.m
+++ basis_functions/fn_basis_gaussian_rbf.m
@@ -3,19 +3,17 @@ Gaussian radial basis function
 
 input
     X: input data
-    c: centre
+    C: centre
     s2: variance
    
 out: 
     BX: gaussian radial basis function of X
 %}
-function BX = fn_basis_gaussian_rbf ( x, c, s2 )
-    D  = distances(c,x);
-    BX = exp(-(0.5/s2)*D);
-end
-
-function D = distances(A,B)
-    m = size(A,2);
-    n = size(B,2);
-    D = repmat( (A.^2)',1,n) + repmat( (B.^2), m,1) - 2*A'*B;
+function BX = fn_basis_gaussian_rbf ( X, C, s2 )    
+    numC= size(C,2) ;
+    numX= size(X,2) ;
+    C2  = sum(C.^2) ;
+    X2  = sum(X.^2) ;    
+    D   = repmat( C2',1, numX ) + repmat( X2, numC, 1) - 2*C'*X;      
+    BX  = exp(-0.5/s2*D) ;
 end
