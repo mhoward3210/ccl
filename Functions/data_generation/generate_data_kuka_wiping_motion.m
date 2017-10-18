c6ed54894df67ebbdc29c7f7d815f57625cc286a
Joao Moura <joaopousa24@gmail.com>
2017-10-18 13:13:49 +0100

    Add comments and user input

--- Functions/data_generation/generate_data_kuka_wiping_motion.m
+++ Functions/data_generation/generate_data_kuka_wiping_motion.m
@@ -1,3 +1,38 @@
+% GENERATE_DATA_KUKA_WIPING_MOTION.m - Generates states (robot
+% joint positions) and actions (robot joint velocities) and time for a
+% circular wiping motion as unconstrained policy and planar surface
+% constraints.
+%   Saves the states and actions in file.
+%
+% Other m-files required: 
+%   getUnconstrainedPolicyRegressors4CircularWipingMotion.m
+%   getTaskRegressors4SurfacePerpendicultaMotionSimulated.m
+%   getConstraintMatrixRegressor4SurfacePerpendicularMotion.m
+%   getConstrainedPolicy.m
+
+% Author: Joao Moura
+% Edinburgh Centre for Robotics, Edinburgh, UK
+% email address: Joao.Moura@ed.ac.uk
+% Website: http://www.edinburgh-robotics.org/students/joao-moura
+% October 2017; Last revision: 18-Oct-2017
+
+%% User Input
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+NDem = 12; % number of demonstrations
+Dangle = 20; % variation of angle in roll and pitch
+% timming:
+tf = 30; % duration of each simulation is seconds
+freq = 30; % number of samples per second
+cutOffTime = 7; % because the initial state of the simulation is not on the
+% constraint, the simulation takes some time until the proportional
+% controller converges the state to the constraint. This initial
+% convergence time is cut out of the training data
+x0 = [0;0;0;pi/2;0;-pi/2;0]; % initial condition
+Kp = 5; % proportional gain
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+
 %% Add path
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
@@ -17,8 +52,12 @@ DH = [0.0, 0.31, 0.0, pi/2; % Robot Kinematic model specified by the Denavit-Har
       0.0, 0.0, 0.0, -pi/2;
       0.0, 0.21-0.132, 0.0, 0.0];
 robot = SerialLink(DH); % Peters Cork robotics library has to be installed
-Phi_A = getConstraintMatrixRegressor4SurfacePerpendicularMotion(robot); % Phi_A(q): vector of regressors for the Constraint matrix as a function of the configuration
-Phi_b = getTaskRegressors4SurfacePerpendicultaMotionSimulated(robot); % Phi_b(q): vector of regressors for the main task as a function of the configuration
+ % Phi_A(x): vector of regressors for the Constraint matrix as a function
+ % of the state
+Phi_A = getConstraintMatrixRegressor4SurfacePerpendicularMotion(robot);
+% Phi_b(x): vector of regressors for the main task as a function of the
+% state
+Phi_b = getTaskRegressors4SurfacePerpendicultaMotionSimulated(robot);
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 
@@ -26,23 +65,17 @@ Phi_b = getTaskRegressors4SurfacePerpendicultaMotionSimulated(robot); % Phi_b(q)
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 fprintf(1,'Generating data ...\n');
-NDem = 12; % number of demonstrations
-q = cell(1, NDem); % configurations
+x = cell(1, NDem); % configurations
 u = cell(1, NDem); % actions
 t = cell(1, NDem); % configuration data
 p = cell(1, NDem); % configurations
 c = cell(1, NDem); % wiping circle centre
 r = cell(1, NDem); % wiping circle radious
 n = cell(1, NDem); % planar surface normal
-Phi = cell(1,NDem);
-policy = cell(1,NDem);
-Dangle = 20; % variation of angle in roll and pitch
-getPos = @(q) transl(robot.fkine(q)); % compute end-effector postion
-% timming:
-tf = 30;
-freq = 30;
+Phi = cell(1,NDem); % policy regressors
+unconstrainedPolicy = cell(1,NDem); % unconstrainedPolicy
 time = linspace(0,tf,tf*freq); % time vector
-timecond = time>7;
+timecond = time>cutOffTime;
 for idx=1:NDem
     % Random variables:
     c{idx} = [rand().*0.15 + 0.45; rand().*0.1-0.05; rand().*0.15+0.35]; % generate random circle centre
@@ -51,30 +84,26 @@ for idx=1:NDem
     pitch = rand()*(2*Dangle) - Dangle;
     T = rpy2tr(roll, pitch, 0); % homogeneous transformation for the end-effector
     n{idx} = T(1:3,3);
-    % initial condition
-    q0 = [0;0;0;pi/2;0;-pi/2;0];
-    % proportional gain
-    Kp = 5;
     % Constant matrices:
     W_A = blkdiag(n{idx}.', n{idx}.', n{idx}.'); % constant gain matrix for the Constraint matrix
     W_b = -Kp*[W_A [-n{idx}.'*c{idx}; 0; 0]];
     % Definition of Constraint matrix and main task
-    A = @(q) W_A*feval(Phi_A,q); % Constraint matrix as a function of configuration
-    b = @(q) W_b*feval(Phi_b,q); % main task as a function of the configuration
+    A = @(x) W_A*feval(Phi_A,x); % Constraint matrix as a function of configuration
+    b = @(x) W_b*feval(Phi_b,x); % main task as a function of the configuration
     % Constrained Policie
     Phi{idx} = getUnconstrainedPolicyRegressors4CircularWipingMotion(robot, c{idx}, r{idx}); % Get regressors for the unconstrained policy
-    policy{idx} = @(q) Phi{idx}(q)*[1; 1];
-    q_dot = getConstrainedPolicy(A, b, policy{idx});
+    unconstrainedPolicy{idx} = @(x) Phi{idx}(x)*[1; 1];
+    x_dot = getConstrainedPolicy(A, b, unconstrainedPolicy{idx});
     % solving motion
-    sol = ode113(@(t,q) q_dot(q),[0 tf], q0);
+    sol = ode113(@(t,x) x_dot(x),[0 tf], x0);
     [traj, dtraj] = deval(sol,time); % evaluation of solution
     % store data
-    q{idx} = num2cell(traj(:,timecond),1);
+    x{idx} = num2cell(traj(:,timecond),1);
     u{idx} = num2cell(dtraj(:,timecond),1);
     timeprov = time(timecond); timeprov = timeprov - timeprov(1);
     t{idx} = num2cell(timeprov,1);
     % computation for plot purposes
-    p{idx}=getPos(traj(:,timecond).');
+    p{idx}=transl(robot.fkine(traj(:,timecond).'));
     disp(idx);
 end
 %--------------------------------------------------------------------------
@@ -83,7 +112,7 @@ end
 %% Save data to file
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
-save('data_simulated.mat','q','u','t');
+save('data_simulated.mat','x','u','t');
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 
@@ -99,7 +128,7 @@ for idx=1:NDem
     plot3(p{idx}(:,1),p{idx}(:,2),p{idx}(:,3),'*r');
     plotCircle3D(c{idx},r{idx},n{idx});
     xlabel('x'); ylabel('y'); zlabel('z');
-    legend('centre','data','policy','circle');
+    legend('centre','data','circle');
     axis equal;
 end
 %--------------------------------------------------------------------------
