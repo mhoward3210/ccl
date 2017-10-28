98fd0f9a910c67904f46bc045f1a16589f1cd8b3
Joao Moura <joaopousa24@gmail.com>
2017-10-28 11:49:55 +0100

    change identation

--- Functions/regressors/getTaskRegressors4SurfacePerpendicularMotionSimulated.m
+++ Functions/regressors/getTaskRegressors4SurfacePerpendicularMotionSimulated.m
@@ -12,26 +12,26 @@ function functionHandle = getTaskRegressors4SurfacePerpendicularMotionSimulated(
 %    functionHandle - function to be evaluated 
 %
 % Example: 
-% % Robot Kinematic model specified by the Denavit-Hartenberg
-% DH = [0.0, 0.31, 0.0, pi/2;
-%       0.0, 0.0, 0.0, -pi/2;
-%       0.0, 0.4, 0.0, -pi/2;
-%       0.0, 0.0, 0.0, pi/2;
-%       0.0, 0.39, 0.0, pi/2;
-%       0.0, 0.0, 0.0, -pi/2;
-%       0.0, 0.21-0.132, 0.0, 0.0];
-% % Peters Cork robotics library has to be installed
-% robot = SerialLink(DH);
-% % Phi_b(x): vector of regressors for the main task as a function of the
-% % state
-% Phi_b = getTaskRegressors4SurfacePerpendicularMotionSimulated(robot);
-% n = [0; 0; 1];
-% W_A = blkdiag(n.', n.', n.'); % constant gain matrix for the Constraint matrix
-% W_b = -5*[W_A [-n.'*[0.4; 0.0; 0.0]; 0; 0]];
-% % main task as a function of the configuration
-% b = @(x) W_b*Phi_b(x);
-% % Constraint matrix for given robot arm configuration
-% disp(b([0;0;0;pi/2;0;-pi/2;0]));
+%     % Robot Kinematic model specified by the Denavit-Hartenberg
+%     DH = [0.0, 0.31, 0.0, pi/2;
+%           0.0, 0.0, 0.0, -pi/2;
+%           0.0, 0.4, 0.0, -pi/2;
+%           0.0, 0.0, 0.0, pi/2;
+%           0.0, 0.39, 0.0, pi/2;
+%           0.0, 0.0, 0.0, -pi/2;
+%           0.0, 0.21-0.132, 0.0, 0.0];
+%     % Peters Cork robotics library has to be installed
+%     robot = SerialLink(DH);
+%     % Phi_b(x): vector of regressors for the main task as a function of the
+%     % state
+%     Phi_b = getTaskRegressors4SurfacePerpendicularMotionSimulated(robot);
+%     n = [0; 0; 1];
+%     W_A = blkdiag(n.', n.', n.'); % constant gain matrix for the Constraint matrix
+%     W_b = -5*[W_A [-n.'*[0.4; 0.0; 0.0]; 0; 0]];
+%     % main task as a function of the configuration
+%     b = @(x) W_b*Phi_b(x);
+%     % Constraint matrix for given robot arm configuration
+%     disp(b([0;0;0;pi/2;0;-pi/2;0]));
 %
 % Libraries required: Peter Corke's Robotics library (MatLab add-on)
 % 
@@ -44,13 +44,13 @@ function functionHandle = getTaskRegressors4SurfacePerpendicularMotionSimulated(
 % October 2017; Last revision: 18-Oct-2017
 
 %------------- BEGIN CODE --------------
-    functionHandle = @Phi_b;
-    function output = Phi_b(q)
-        T = robotHandle.fkine(q); % end-effector homogeneous transformation
-        tT = reshape(transl(TT),[],1); % end-effector position
-        rot = t2r(T); % end-effector orientation (rotation matrix)
-        xT = rot(:,1); yT = rot(:,2); % Unit vectors that define the plane perpendicular to end-effector
-        output = [tT; xT; yT; 1];
-    end
+functionHandle = @Phi_b;
+function output = Phi_b(q)
+    T = robotHandle.fkine(q); % end-effector homogeneous transformation
+    tT = reshape(transl(TT),[],1); % end-effector position
+    rot = t2r(T); % end-effector orientation (rotation matrix)
+    xT = rot(:,1); yT = rot(:,2); % Unit vectors that define the plane perpendicular to end-effector
+    output = [tT; xT; yT; 1];
+end
 %------------- END OF CODE --------------
 end
