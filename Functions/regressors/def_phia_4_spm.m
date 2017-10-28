c18706c8bca8629765b78400964a902a8a7404e8
Joao Moura <joaopousa24@gmail.com>
2017-10-28 18:39:42 +0100

    Improve comments and documentation

--- Functions/regressors/def_phia_4_spm.m
+++ Functions/regressors/def_phia_4_spm.m
@@ -1,7 +1,15 @@
 function functionHandle = def_phia_4_spm(robotHandle)
-%GETCONSTRAINEDMATRIXREGRESSORS4SREFACEPERPENDICULARMOTION - Get regressors 
-% for the constraint matrix when representing the constraint matrix as the 
-% product of a vector of regressors by a constant matrix.
+% Define suitable constraint matrix regressors for a surface perpendicular motion.
+%
+% Consider a set of Constraints defined as a linear combination of regressors:
+%
+%         A(x) = W * Phi_A(x),
+%
+% where W is a matrix of weights, and Phi_A(x) is a matrix of regressors.
+% def_phia_4_spm returns a MatLab function handle to a set of regressors
+% suitable for the constraint of moving the robot end-effector in contact 
+% and perpendicular to a surface.
+% This regressors are a function of the robot configuration - column vector.
 %
 % Syntax:  functionHandle = def_phia_4_spm(robotHandle)
 %
@@ -9,10 +17,11 @@ function functionHandle = def_phia_4_spm(robotHandle)
 %    robotHandle - Peter Corke's Serial-link robot class
 %
 % Outputs:
-%    functionHandle - function to be evaluated 
+%    functionHandle - MatLab function handle with robot configuration 
+%                     (column vector) as input
 %
 % Example: 
-%     % Robot Kinematic model specified by the Denavit-Hartenberg
+%     % Robot Kinematic model specified by the Denavit-Hartenberg.
 %     DH = [0.0, 0.31, 0.0, pi/2;
 %           0.0, 0.0, 0.0, -pi/2;
 %           0.0, 0.4, 0.0, -pi/2;
@@ -20,37 +29,37 @@ function functionHandle = def_phia_4_spm(robotHandle)
 %           0.0, 0.39, 0.0, pi/2;
 %           0.0, 0.0, 0.0, -pi/2;
 %           0.0, 0.21-0.132, 0.0, 0.0];
-%     % Peters Cork robotics library has to be installed
+%     % Peters Cork robotics library has to be installed:
 %     robot = SerialLink(DH);
 %     % Phi_A(x): vector of regressors for the Constraint matrix as a function
 %     % of the state
-%     n = [0; 0; 1];
-%     W_A = blkdiag(n.', n.', n.'); % constant gain matrix for the Constraint matrix
-%     PhiA = def_phia_4_spm(robot);
-%     % Constraint matrix as a function of configuration
+%     n = [0; 0; 1]; % Cartesian normal of the constraint surface.
+%     W_A = blkdiag(n.', n.', n.'); % Constant gain matrix for the Constraint matrix.
+%     Phi_A = def_phia_4_spm(robot);
+%     % Constraint matrix as a function of configuration.
 %     A = @(x) W_A*PhiA(x);
-%     % Constraint matrix for given robot arm configuration
+%     % Constraint matrix for given robot arm configuration:
 %     disp(A([0;0;0;pi/2;0;-pi/2;0]));
 %
 % Libraries required: Peter Corke's Robotics library (MatLab add-on)
 % 
-% See also: GETTASKREGRESSORS4SRFACEPERTENDCULARMOTIONSIMULATED
+% See also: 
 
 % Author: Joao Moura
 % Edinburgh Centre for Robotics, Edinburgh, UK
 % email address: Joao.Moura@ed.ac.uk
 % Website: http://www.edinburgh-robotics.org/students/joao-moura
-% October 2017; Last revision: 18-Oct-2017
+% October 2017; Last revision: 28-Oct-2017
 
 %------------- BEGIN CODE --------------
 functionHandle = @Phi_A;
 function output = Phi_A(q)
-    J = robotHandle.jacob0(q); % Robot Jacobian in the global reference frame
-    JtT = J(1:3,:); % Jacobian for the end-effector position
-    Jrot = J(4:6,:); % rotation component of Jacobian
-    rot = t2r(robotHandle.fkine(q)); % end-effector orientation (rotation matrix)
-    xT = rot(:,1); yT = rot(:,2); % Unit vectors that define the plane perpendicular to end-effector
-    JxT = -skew(xT)*Jrot; JyT = -skew(yT)*Jrot; % Jacobians for the end-effector frame unit vectors
+    J = robotHandle.jacob0(q); % Robot Jacobian in the global reference frame.
+    JtT = J(1:3,:); % Jacobian for the end-effector position.
+    Jrot = J(4:6,:); % Rotation component of Jacobian.
+    rot = t2r(robotHandle.fkine(q)); % End-effector orientation (rotation matrix).
+    xT = rot(:,1); yT = rot(:,2); % Unit vectors that define the plane perpendicular to end-effector.
+    JxT = -skew(xT)*Jrot; JyT = -skew(yT)*Jrot; % Jacobians for the end-effector frame unit vectors.
     output = [JtT; JxT; JyT];
 end
 %------------- END OF CODE --------------
