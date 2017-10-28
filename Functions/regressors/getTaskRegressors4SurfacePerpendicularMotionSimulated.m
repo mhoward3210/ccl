5ddaf3e9d7fe6ad47b81852a8f491a487155f588
Joao Moura <joaopousa24@gmail.com>
2017-10-28 12:12:48 +0100

    bug fix: wrong variable name

--- Functions/regressors/getTaskRegressors4SurfacePerpendicularMotionSimulated.m
+++ Functions/regressors/getTaskRegressors4SurfacePerpendicularMotionSimulated.m
@@ -47,7 +47,7 @@ function functionHandle = getTaskRegressors4SurfacePerpendicularMotionSimulated(
 functionHandle = @Phi_b;
 function output = Phi_b(q)
     T = robotHandle.fkine(q); % end-effector homogeneous transformation
-    tT = reshape(transl(TT),[],1); % end-effector position
+    tT = reshape(transl(T),[],1); % end-effector position
     rot = t2r(T); % end-effector orientation (rotation matrix)
     xT = rot(:,1); yT = rot(:,2); % Unit vectors that define the plane perpendicular to end-effector
     output = [tT; xT; yT; 1];
