73825c86bfabcf8a9e2ff57e87beaa8ad40bab72
Joao Moura <joaopousa24@gmail.com>
2017-11-02 14:55:41 +0000

    Use cellfun to evaluate H

--- Functions/learn_constraint/getClosedFormNullSpaceProjectionMatrixEstimatior.m
+++ Functions/learn_constraint/getClosedFormNullSpaceProjectionMatrixEstimatior.m
@@ -3,18 +3,14 @@ function functionHandle = getClosedFormNullSpaceProjectionMatrixEstimatior(Phi_A
 
     function [nullSpaceProjectionHat, WA_hat, Wb_hat] = ClosedFormNullSpaceProjectionMatrixEstimatior(q, u)
         % number of the regressors of the constraint function
-        Ndata = length(q);
         Ndof = length(q{1});
         NA = size(Phi_A(zeros(Ndof,1)),1);
-        Nb = size(Phi_b(zeros(Ndof,1)),1); 
         % Evaluate constraint and task regressors for all the data
-        HH = zeros(NA+Nb,Ndata);
-        parfor idx=1:Ndata
-            % H(q,u): Matrix of regressors as a function of the configuration and action compute number of regressors
-            HH(:,idx) = [feval(Phi_A, q{idx})*u{idx}; -feval(Phi_b,q{idx})];
-        end
+        H = @(q,u) [Phi_A(q)*u; -Phi_b(q)];
+        H_cell = cellfun(H, q, u, 'UniformOutput', false);
+        H_matrix = cell2mat(H_cell);
         % Singular value decomposition to estimate the gains
-        [U,~,~]=svd(HH);
+        [U,~,~]=svd(H_matrix);
         W_hat = U(:,(end-(ConstraintDim-1)):end).'; % select the last ConstraintDim columns  
         WA_hat = W_hat(:,1:NA);
         Wb_hat = W_hat(:,(NA+1):end);
