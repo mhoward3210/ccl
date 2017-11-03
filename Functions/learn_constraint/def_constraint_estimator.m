416b9ed9d2967e44ff7d96baf3193575474a7f54
Joao Moura <joaopousa24@gmail.com>
2017-11-03 17:23:23 +0000

    Add option: stationary or action force constrained system

--- Functions/learn_constraint/def_constraint_estimator.m
+++ Functions/learn_constraint/def_constraint_estimator.m
@@ -6,6 +6,7 @@ function functionHandle = def_constraint_estimator(Phi_A, Phi_b, varargin)
     defaultConstraintDim = 1;
     defaultSystemType = 'forced_action';
     expectedSystemTypes = {'forced_action','stationary'};
+    expectedH = {@(q,u) [Phi_A(q)*u; -Phi_b(q)], @(q,u) Phi_A(q)*u};
 
     p = inputParser;
     p.CaseSensitive = true;
@@ -16,20 +17,19 @@ function functionHandle = def_constraint_estimator(Phi_A, Phi_b, varargin)
     parse(p,varargin{:});
     
     ConstraintDim = p.Results.constraint_dim;
-    %SystemType = p.Results.system_type;
+    systemType = p.Results.system_type;
+    H = expectedH{strcmp(systemType,expectedSystemTypes)};
 
     function [nullSpaceProjectionHat, H_cell, W_hat] = ClosedFormNullSpaceProjectionMatrixEstimatior(q, u)
-        % number of the regressors for the constraint matrix
-        NA = size(Phi_A(q{1}),1);
-        % Evaluate constraint and task regressors for all the data
-        H = @(q,u) [Phi_A(q)*u; -Phi_b(q)];
+        % Evaluate H regressors for given the data set
         H_cell = cellfun(H, q, u, 'UniformOutput', false);
         H_matrix = cell2mat(H_cell);
         % Singular value decomposition to estimate the gains
         [U,~,~]=svd(H_matrix);
-        W_hat = U(:,(end-(ConstraintDim-1)):end).'; % select the last ConstraintDim columns  
+        W_hat = U(:,(end-(ConstraintDim-1)):end).'; % select the last ConstraintDim columns 
+        NA = size(Phi_A(q{1}),1); % number of the regressors for the constraint matrix
         WA_hat = W_hat(:,1:NA);
-        % Definition of Constraint matrix and main task
+        % Definition of Constraint matrix
         A_hat = @(q) WA_hat*Phi_A(q); % Constraint matrix as a function of configuration
         nullSpaceProjectionHat = def_null_space_proj(A_hat);
     end
